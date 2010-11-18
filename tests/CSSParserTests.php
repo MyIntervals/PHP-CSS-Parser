f1eab3e5e1c62aa2717a4b1619daab8f6f437989
Raphael Schweikert <raphael.schweikert@stud.unibas.ch>
2010-11-18 12:48:51 +0100

    added test for color parsing

--- tests/CSSParserTests.php
+++ tests/CSSParserTests.php
@@ -25,4 +25,45 @@ class CSSParserTests extends PHPUnit_Framework_TestCase {
 			closedir($rHandle);
 		}
 	}
+	
+	/**
+	* @depends testCssFiles
+	*/
+	function testColorParsing() {
+		$oDoc = $this->parsedStructureForFile('colortest');
+		foreach($oDoc->getAllRuleSets() as $oRuleSet) {
+			if(!$oRuleSet instanceof CSSSelector) {
+				continue;
+			}
+			$aSelector = $oRuleSet->getSelector();
+			if($aSelector[0] === '#mine') {
+				$aColorRule = $oRuleSet->getRules('color');
+				$aValues = $aColorRule['color']->getValues();
+				$this->assertSame('red', $aValues[0][0]);
+				$aColorRule = $oRuleSet->getRules('background-');
+				$aValues = $aColorRule['background-color']->getValues();
+				$this->assertEquals(array('r' => new CSSSize(35.0), 'g' => new CSSSize(35.0), 'b' => new CSSSize(35.0)), $aValues[0][0]->getColor());
+				$aColorRule = $oRuleSet->getRules('border-color');
+				$aValues = $aColorRule['border-color']->getValues();
+				$this->assertEquals(array('r' => new CSSSize(10.0), 'g' => new CSSSize(100.0), 'b' => new CSSSize(230.0), 'a' => new CSSSize(0.3)), $aValues[0][0]->getColor());
+				$aColorRule = $oRuleSet->getRules('outline-color');
+				$aValues = $aColorRule['outline-color']->getValues();
+				$this->assertEquals(array('r' => new CSSSize(34.0), 'g' => new CSSSize(34.0), 'b' => new CSSSize(34.0)), $aValues[0][0]->getColor());
+			}
+		}
+		foreach($oDoc->getAllValues('background-') as $oColor) {
+			if($oColor->getColorDescription() === 'hsl') {
+				$this->assertEquals(array('h' => new CSSSize(220.0), 's' => new CSSSize(10.0), 'l' => new CSSSize(220.0)), $oColor->getColor());
+			}
+		}
+		foreach($oDoc->getAllValues('color') as $sColor) {
+			$this->assertSame('red', $sColor);
+		}
+	}
+	
+	function parsedStructureForFile($sFileName) {
+		$sFile = dirname(__FILE__).DIRECTORY_SEPARATOR.'files'.DIRECTORY_SEPARATOR."$sFileName.css";
+		$oParser = new CSSParser(file_get_contents($sFile));
+		return $oParser->parse();
+	}
 }
