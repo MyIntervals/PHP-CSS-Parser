1d54e8c2c854d9f7fdcf2736873ebc49a621d5f8
ju1ius <jules.bernable@laposte.net>
2011-09-11 07:17:24 +0200

    allow import of absolute urls when parsing a local file

--- CSSParser.php
+++ CSSParser.php
@@ -157,7 +157,8 @@ class CSSParser {
       if($this->aOptions['resolve_imports']) {
         $parser = new CSSParser($aImportOptions);
         $sPath = $oImport->getLocation()->getURL()->getString();
-        if($this->sImportMode == self::IMPORT_URL) {
+        $bIsAbsUrl = CSSUrlUtils::isAbsUrl($sPath);
+        if($this->sImportMode == self::IMPORT_URL || $bIsAbsUrl) {
           if(!in_array($sPath, $this->aLoadedFiles)) {          
             $oImportedDoc = $parser->parseURL($sPath, $this->aLoadedFiles);
             $this->aLoadedFiles = $parser->getLoadedFiles();
