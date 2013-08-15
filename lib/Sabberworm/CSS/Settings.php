67e025b266e71e9892801f9b532655aac9cd8954
vladkens <vladkens@yandex.ru>
2013-08-15 17:56:55 +0300

    Uses multibyte string only if loaded mbstring extension.

--- lib/Sabberworm/CSS/Settings.php
+++ lib/Sabberworm/CSS/Settings.php
@@ -11,9 +11,9 @@ use Sabberworm\CSS\Rule\Rule;
  */
 class Settings {
 	/**
-	* Multi-byte string support. If true (default), will use (slower) mb_strlen, mb_convert_case, mb_substr and mb_strpos functions. Otherwise, the normal (ASCII-Only) functions will be used.
+	* Multi-byte string support. If true (mbstring extension must be enabled), will use (slower) mb_strlen, mb_convert_case, mb_substr and mb_strpos functions. Otherwise, the normal (ASCII-Only) functions will be used.
 	*/
-	public $bMultibyteSupport = true;
+	public $bMultibyteSupport;
 
 	/**
 	* The default charset for the CSS if no `@charset` rule is found. Defaults to utf-8.
@@ -25,7 +25,9 @@ class Settings {
 	*/
 	public $bLenientParsing = true;
 
-	private function __construct() {}
+	private function __construct() {
+        $this->bMultibyteSupport = extension_loaded('mbstring');
+    }
 
 	public static function create() {
 		return new Settings();
