MyIntervals/PHP-CSS-Parser

commit 6077052f23cefee6257f60b1ba3e28d6621e0cac

More efficient sizeUnits determination

lib/Sabberworm/CSS/Parser.php
@@ -43,7 +43,16 @@ class Parser {
 		}
 		$this->oParserSettings = $oParserSettings;
 		$this->blockRules = explode('/', AtRule::BLOCK_RULES);
-		$this->sizeUnits = explode('/', Size::ABSOLUTE_SIZE_UNITS.'/'.Size::RELATIVE_SIZE_UNITS.'/'.Size::NON_SIZE_UNITS);
+
+		foreach (explode('/', Size::ABSOLUTE_SIZE_UNITS.'/'.Size::RELATIVE_SIZE_UNITS.'/'.Size::NON_SIZE_UNITS) as $val) {
+			$size = strlen($val);
+			if (isset($this->sizeUnits[$size])) {
+				$this->sizeUnits[$size][] = $val;
+			} else {
+				$this->sizeUnits[$size] = array($val);
+			}
+		}
+		ksort($this->sizeUnits, SORT_NUMERIC);
 	}
 
 	public function setCharset($sCharset) {
@@ -397,11 +406,12 @@ class Parser {
 				$sSize .= $this->consume(1);
 			}
 		}
+
 		$sUnit = null;
-		foreach($this->sizeUnits as $sDefinedUnit) {
-			if ($this->comes($sDefinedUnit, true)) {
-				$sUnit = $sDefinedUnit;
-				$this->consume($sDefinedUnit);
+		foreach ($this->sizeUnits as $len => $val) {
+			if (($pos = array_search($this->peek($len), $val)) !== false) {
+				$sUnit = $val[$pos];
+				$this->consume($len);
 				break;
 			}
 		}

commit 08a0ed0c2f8e0c4a82c81756bc689a1316ac54df

Fix case-insensitive Size unit parsing
This fixes a regression introduced in 6077052f23cefee6257f60b1ba3e28d6621e0cac

lib/Sabberworm/CSS/Parser.php
@@ -33,7 +33,7 @@ class Parser {
 	private $iLength;
 	private $peekCache = null;
 	private $blockRules;
-	private $sizeUnits;
+	private $aSizeUnits;
 
 	public function __construct($sText, Settings $oParserSettings = null) {
 		$this->sText = $sText;
@@ -45,14 +45,13 @@ class Parser {
 		$this->blockRules = explode('/', AtRule::BLOCK_RULES);
 
 		foreach (explode('/', Size::ABSOLUTE_SIZE_UNITS.'/'.Size::RELATIVE_SIZE_UNITS.'/'.Size::NON_SIZE_UNITS) as $val) {
-			$size = strlen($val);
-			if (isset($this->sizeUnits[$size])) {
-				$this->sizeUnits[$size][] = $val;
-			} else {
-				$this->sizeUnits[$size] = array($val);
+			$iSize = strlen($val);
+			if(!isset($this->aSizeUnits[$iSize])) {
+				$this->aSizeUnits[$iSize] = array();
 			}
+			$this->aSizeUnits[$iSize][strtolower($val)] = $val;
 		}
-		ksort($this->sizeUnits, SORT_NUMERIC);
+		ksort($this->aSizeUnits, SORT_NUMERIC);
 	}
 
 	public function setCharset($sCharset) {
@@ -408,10 +407,9 @@ class Parser {
 		}
 
 		$sUnit = null;
-		foreach ($this->sizeUnits as $len => $val) {
-			if (($pos = array_search($this->peek($len), $val)) !== false) {
-				$sUnit = $val[$pos];
-				$this->consume($len);
+		foreach ($this->aSizeUnits as $iLength => &$aValues) {
+			if(($sUnit = @$aValues[strtolower($this->peek($iLength))]) !== null) {
+				$this->consume($iLength);
 				break;
 			}
 		}
@@ -469,11 +467,11 @@ class Parser {
 			?: preg_match("/^(-\\w+-)?$sMatch$/i", $sIdentifier) === 1;
 	}
 
-	private function comes($sString, $alpha = false) {
-		$sPeek = $this->peek($alpha ? $this->strlen($sString) : strlen($sString));
+	private function comes($sString, $bCaseInsensitive = false) {
+		$sPeek = $this->peek(strlen($sString));
 		return ($sPeek == '')
 			? false
-			: $this->streql($sPeek, $sString, $alpha);
+			: $this->streql($sPeek, $sString, $bCaseInsensitive);
 	}
 
 	private function peek($iLength = 1, $iOffset = 0) {