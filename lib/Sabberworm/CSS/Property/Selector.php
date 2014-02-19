2cc55a2882b742de4553240c7883b76c1fd031ce
Raphael Schweikert <any@sabberworm.com>
2014-02-19 14:10:10 +0100

    Update Selector.php
    Initialize $aMatches

--- lib/Sabberworm/CSS/Property/Selector.php
+++ lib/Sabberworm/CSS/Property/Selector.php
@@ -7,34 +7,33 @@ namespace Sabberworm\CSS\Property;
  */
 class Selector {
 
-	const
-			NON_ID_ATTRIBUTES_AND_PSEUDO_CLASSES_RX = '/
-			(\.[\w]+)										 # classes
-			|
-			\[(\w+)											 # attributes
-			|
-			(\:(												 # pseudo classes
-				link|visited|active
-				|hover|focus
-				|lang
-				|target
-				|enabled|disabled|checked|indeterminate
-				|root
-				|nth-child|nth-last-child|nth-of-type|nth-last-of-type
-				|first-child|last-child|first-of-type|last-of-type
-				|only-child|only-of-type
-				|empty|contains
-			))
-		/ix',
-			ELEMENTS_AND_PSEUDO_ELEMENTS_RX = '/
-			((^|[\s\+\>\~]+)[\w]+ # elements
-			|										
-			\:{1,2}(								 # pseudo-elements
-				after|before
-				|first-letter|first-line
-				|selection
-			)
-		)/ix';
+	//Regexes for specificity calculations
+	const NON_ID_ATTRIBUTES_AND_PSEUDO_CLASSES_RX = '/
+	(\.[\w]+)                   # classes
+	|
+	\[(\w+)                     # attributes
+	|
+	(\:(                        # pseudo classes
+		link|visited|active
+		|hover|focus
+		|lang
+		|target
+		|enabled|disabled|checked|indeterminate
+		|root
+		|nth-child|nth-last-child|nth-of-type|nth-last-of-type
+		|first-child|last-child|first-of-type|last-of-type
+		|only-child|only-of-type
+		|empty|contains
+	))
+	/ix';
+
+	const ELEMENTS_AND_PSEUDO_ELEMENTS_RX = '/
+	((^|[\s\+\>\~]+)[\w]+   # elements
+	|
+	\:{1,2}(                # pseudo-elements
+		after|before|first-letter|first-line|selection
+	))
+	/ix';
 
 	private $sSelector;
 	private $iSpecificity;
@@ -63,7 +62,7 @@ class Selector {
 		if ($this->iSpecificity === null) {
 			$a = 0;
 			/// @todo should exclude \# as well as "#"
-			$aMatches;
+			$aMatches = null;
 			$b = substr_count($this->sSelector, '#');
 			$c = preg_match_all(self::NON_ID_ATTRIBUTES_AND_PSEUDO_CLASSES_RX, $this->sSelector, $aMatches);
 			$d = preg_match_all(self::ELEMENTS_AND_PSEUDO_ELEMENTS_RX, $this->sSelector, $aMatches);
