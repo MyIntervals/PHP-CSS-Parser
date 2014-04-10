d0b68700c87b447eae3bc27610c5698b6e3ad5e5
Raphael Schweikert <any@sabberworm.com>
2014-04-10 18:40:04 +0200

    Add `fr` relative size unit
    defined in the CSS3 Grid Layout Module
    
    http://www.w3.org/TR/css3-grid-layout/#fr-unit

--- lib/Sabberworm/CSS/Value/Size.php
+++ lib/Sabberworm/CSS/Value/Size.php
@@ -5,7 +5,7 @@ namespace Sabberworm\CSS\Value;
 class Size extends PrimitiveValue {
 
 	const ABSOLUTE_SIZE_UNITS = 'px/cm/mm/mozmm/in/pt/pc/vh/vw/vm/vmin/vmax/rem'; //vh/vw/vm(ax)/vmin/rem are absolute insofar as they don’t scale to the immediate parent (only the viewport)
-	const RELATIVE_SIZE_UNITS = '%/em/ex/ch';
+	const RELATIVE_SIZE_UNITS = '%/em/ex/ch/fr';
 	const NON_SIZE_UNITS = 'deg/grad/rad/s/ms/turns/Hz/kHz';
 
 	private $fSize;
