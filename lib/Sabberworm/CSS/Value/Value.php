696e85a1a32f7f64ffd2d92d3e40c3aedb9d80ce
Raphael Schweikert <any@sabberworm.com>
2014-09-18 13:00:33 +0200

    Remove abstract methods defined in interfaces
    Causes a fatal error in PHP < 5.3.9
    
    Closes #83 as fixed

--- lib/Sabberworm/CSS/Value/Value.php
+++ lib/Sabberworm/CSS/Value/Value.php
@@ -5,6 +5,7 @@ namespace Sabberworm\CSS\Value;
 use Sabberworm\CSS\Renderable;
 
 abstract class Value implements Renderable {
-	public abstract function __toString();
-	public abstract function render(\Sabberworm\CSS\OutputFormat $oOutputFormat);
+	//Methods are commented out because re-declaring them here is a fatal error in PHP < 5.3.9
+	//public abstract function __toString();
+	//public abstract function render(\Sabberworm\CSS\OutputFormat $oOutputFormat);
 }
