394c448cc5cc36ad88ca223f4e54adbfacf9c23a
Frederic Massart <fred@moodle.com>
2016-08-01 19:17:17 +0800

    Support for inserting an item in a CSSList

--- tests/Sabberworm/CSS/CSSList/DocumentTest.php
+++ tests/Sabberworm/CSS/CSSList/DocumentTest.php
@@ -2,6 +2,7 @@
 
 namespace Sabberworm\CSS\CSSList;
 
+use Sabberworm\CSS\RuleSet\DeclarationBlock;
 use Sabberworm\CSS\Parser;
 
 class DocumentTest extends \PHPUnit_Framework_TestCase {
@@ -21,6 +22,38 @@ class DocumentTest extends \PHPUnit_Framework_TestCase {
 		$oDoc->setContents(array($aContents[0], $aContents2[0]));
 		$aFinalContents = $oDoc->getContents();
 		$this->assertCount(2, $aFinalContents);
+	}
+
+	public function testInsertContent() {
+		$sCss = '.thing { left: 10px; } .stuff { margin: 1px; } ';
+		$oParser = new Parser($sCss);
+		$oDoc = $oParser->parse();
+		$aContents = $oDoc->getContents();
+		$this->assertCount(2, $aContents);
+
+		$oThing = $aContents[0];
+		$oStuff = $aContents[1];
+
+		$oFirst = new DeclarationBlock();
+		$oFirst->setSelectors('.first');
+		$oBetween = new DeclarationBlock();
+		$oBetween->setSelectors('.between');
+		$oOrphan = new DeclarationBlock();
+		$oOrphan->setSelectors('.forever-alone');
+		$oNotFound = new DeclarationBlock();
+		$oNotFound->setSelectors('.not-found');
+
+		$oDoc->insert($oFirst, $oThing);
+		$oDoc->insert($oBetween, $oStuff);
+		$oDoc->insert($oOrphan, $oNotFound);
+
+		$aContents = $oDoc->getContents();
+		$this->assertCount(5, $aContents);
+		$this->assertSame($oFirst, $aContents[0]);
+		$this->assertSame($oThing, $aContents[1]);
+		$this->assertSame($oBetween, $aContents[2]);
+		$this->assertSame($oStuff, $aContents[3]);
+		$this->assertSame($oOrphan, $aContents[4]);
 	}
 
 }
