aa39efd03a067c53f00df23ff51df5ad6e2a76d1
raxbg <crownandthering@gmail.com>
2018-08-21 17:34:56 +0300

    Bug fix in calc parsing when the first operand is a negative value

--- tests/Sabberworm/CSS/ParserTest.php
+++ tests/Sabberworm/CSS/ParserTest.php
@@ -390,6 +390,7 @@ body {background-url: url("http://somesite.com/images/someimage.gif");}';
 	function testCalcInFile() {
 		$oDoc = $this->parsedStructureForFile('calc', Settings::create()->withMultibyteSupport(true));
 		$sExpected = 'div {width: calc(100% / 4);}
+div {margin-top: calc(-120% - 4px);}
 div {height: -webkit-calc(9 / 16 * 100%) !important;width: -moz-calc(( 50px - 50% ) * 2);}';
 		$this->assertSame($sExpected, $oDoc->render());
 	}
