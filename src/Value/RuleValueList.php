dfac263e8c4444986ef22aa57a75cf058ae2fd40
Oliver Klee <github@oliverklee.de>
2021-06-27 09:53:02 +0200

    Add type annotations for `RuleValueList`

--- src/Value/RuleValueList.php
+++ src/Value/RuleValueList.php
@@ -4,6 +4,10 @@ namespace Sabberworm\CSS\Value;
 
 class RuleValueList extends ValueList
 {
+    /**
+     * @param string $sSeparator
+     * @param int $iLineNo
+     */
     public function __construct($sSeparator = ',', $iLineNo = 0)
     {
         parent::__construct([], $sSeparator, $iLineNo);
