2e928ecb920ced81a56d056761f800d10d2e22bd
Oliver Klee <github@oliverklee.de>
2021-07-02 22:03:04 +0200

    Add type annotations for `AtRule`

--- src/Property/AtRule.php
+++ src/Property/AtRule.php
@@ -7,10 +7,19 @@ use Sabberworm\CSS\Renderable;
 
 interface AtRule extends Renderable, Commentable
 {
-    // Since there are more set rules than block rules,
-    // we’re whitelisting the block rules and have anything else be treated as a set rule.
+    /**
+     * Since there are more set rules than block rules,
+     * we’re whitelisting the block rules and have anything else be treated as a set rule.
+     *
+     * @var string
+     */
     const BLOCK_RULES = 'media/document/supports/region-style/font-feature-values';
-    // … and more font-specific ones (to be used inside font-feature-values)
+
+    /**
+     * … and more font-specific ones (to be used inside font-feature-values)
+     *
+     * @var string
+     */
     const SET_RULES = 'font-face/counter-style/page/swash/styleset/annotation';
 
     /**
