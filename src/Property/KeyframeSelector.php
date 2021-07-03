b7f12f4e62ec8d478441448461a9de1ea30e60a2
Oliver Klee <github@oliverklee.de>
2021-07-03 12:53:46 +0200

    Use spaces for indenting regular expressions
    There is no need to use tabs here, and now the indentation is
    consistent with the indentation used for the PHP code.

--- src/Property/KeyframeSelector.php
+++ src/Property/KeyframeSelector.php
@@ -10,14 +10,14 @@ class KeyframeSelector extends Selector
      * @var string
      */
     const SELECTOR_VALIDATION_RX = '/
-	^(
-		(?:
-			[a-zA-Z0-9\x{00A0}-\x{FFFF}_^$|*="\'~\[\]()\-\s\.:#+>]* # any sequence of valid unescaped characters
-			(?:\\\\.)?                                              # a single escaped character
-			(?:([\'"]).*?(?<!\\\\)\2)?                              # a quoted text like [id="example"]
-		)*
-	)|
-	(\d+%)                                                          # keyframe animation progress percentage (e.g. 50%)
-	$
-	/ux';
+    ^(
+        (?:
+            [a-zA-Z0-9\x{00A0}-\x{FFFF}_^$|*="\'~\[\]()\-\s\.:#+>]* # any sequence of valid unescaped characters
+            (?:\\\\.)?                                              # a single escaped character
+            (?:([\'"]).*?(?<!\\\\)\2)?                              # a quoted text like [id="example"]
+        )*
+    )|
+    (\d+%)                                                          # keyframe animation progress percentage (e.g. 50%)
+    $
+    /ux';
 }
