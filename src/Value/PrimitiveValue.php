a42d909e23c5136c6994e831c2cf1d051e6d63e9
Oliver Klee <github@oliverklee.de>
2021-07-06 00:15:32 +0200

    Add type annotations for `PrimitiveValue`

--- src/Value/PrimitiveValue.php
+++ src/Value/PrimitiveValue.php
@@ -4,6 +4,9 @@ namespace Sabberworm\CSS\Value;
 
 abstract class PrimitiveValue extends Value
 {
+    /**
+     * @param int $iLineNo
+     */
     public function __construct($iLineNo = 0)
     {
         parent::__construct($iLineNo);
