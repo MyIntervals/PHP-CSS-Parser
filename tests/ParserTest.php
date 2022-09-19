8d7147ff92264b162bfcaeecfa08efcaf23f4802
Ivailo Hristov <crownandthering@gmail.com>
2022-09-19 19:00:08 +0300

    [FEATURE] Handle arithmetic operators in Value

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -1197,10 +1197,10 @@ body {background-color: red;}';
     public function functionArithmeticInFile()
     {
         $oDoc = self::parsedStructureForFile('function-arithmetic', Settings::create()->withMultibyteSupport(true));
-        $sExpected = 'div {height: max(300,vh+10);}
-div {height: max(300,vh-10);}
-div {height: max(300,vh*10);}
-div {height: max(300,vh/10);}';
+        $sExpected = 'div {height: max(300,vh + 10);}
+div {height: max(300,vh - 10);}
+div {height: max(300,vh * 10);}
+div {height: max(300,vh / 10);}';
         self::assertSame($sExpected, $oDoc->render());
     }
 
