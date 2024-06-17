739fd9957fc7199806360a38018ecdd1e43d0626
Daniel Ziegenberg <daniel@ziegenberg.at>
2024-06-17 21:21:11 +0200

    fixup! [TASK] rector: add php70 rule set

--- config/rector.php
+++ config/rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
@@ -12,7 +13,7 @@ return RectorConfig::configure()
             __DIR__ . '/../tests',
         ]
     )
-    ->withPhpSets(php70: true)
+    ->withSets([SetList::PHP_70])
     ->withRules(
         [
             // AddVoidReturnTypeWhereNoReturnRector::class,
