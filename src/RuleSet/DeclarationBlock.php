dc5f0e9145d3c90d181e903f6bfeab893624ad80
Daniel Ziegenberg <daniel@ziegenberg.at>
2024-06-19 19:19:33 +0200

    [TASK] Deprecate DeclarationBlock::createListStyleShorthand (#577)
    Signed-off-by: Daniel Ziegenberg <daniel@ziegenberg.at>

--- src/RuleSet/DeclarationBlock.php
+++ src/RuleSet/DeclarationBlock.php
@@ -605,6 +605,8 @@ class DeclarationBlock extends RuleSet
 
     /**
      * @return void
+     *
+     * @deprecated This will be removed without substitution in version 10.0.
      */
     public function createListStyleShorthand()
     {
