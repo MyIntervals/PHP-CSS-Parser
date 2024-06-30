MyIntervals/PHP-CSS-Parser

commit 93835bc14f86221af32f2d91f4266c528f0363e0

[TASK] Update PHP-CS-Fixer (#592)
Also explicitly disallow trailing comma in function call arguments (requires PHP >= 7.4) and function declaration parameters (requires PHP >= 8.0), since the default has changed.

config/php-cs-fixer.php
@@ -1,8 +1,6 @@
 <?php
 
-if (PHP_SAPI !== 'cli') {
-    die('This script supports command line usage only. Please check your command.');
-}
+declare(strict_types=1);
 
 return (new \PhpCsFixer\Config())
     ->setRiskyAllowed(true)
@@ -20,6 +18,9 @@ return (new \PhpCsFixer\Config())
             '@PHPUnit60Migration:risky' => true,
             '@PHPUnit75Migration:risky' => true,
             '@PHPUnit84Migration:risky' => true,
+
+            // overwrite the PER2 defaults to restore compatibility with PHP 7.x
+            'trailing_comma_in_multiline' => ['elements' => ['arrays', 'match']],
 
             'php_unit_construct' => true,
             'php_unit_dedicate_assert' => ['target' => 'newest'],

.phive/phars.xml
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <phive xmlns="https://phar.io/phive">
-  <phar name="php-cs-fixer" version="^3.50.0" installed="3.50.0" location="./.phive/php-cs-fixer" copy="false"/>
+  <phar name="php-cs-fixer" version="^3.59.3" installed="3.59.3" location="./.phive/php-cs-fixer" copy="false"/>
 </phive>

commit 27aa880d245e8428a7096c76ba9ad6a4bb867e17

[TASK] Remove another PHP-8-only setting from the PHP-CS-Fixer config (#629)
Resolves problem introduced by #592.

config/php-cs-fixer.php
@@ -20,7 +20,7 @@ return (new \PhpCsFixer\Config())
             '@PHPUnit84Migration:risky' => true,
 
             // overwrite the PER2 defaults to restore compatibility with PHP 7.x
-            'trailing_comma_in_multiline' => ['elements' => ['arrays', 'match']],
+            'trailing_comma_in_multiline' => ['elements' => ['arrays']],
 
             // for `in_array` & friends
             'strict_param' => true,