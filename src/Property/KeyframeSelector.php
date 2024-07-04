7b461cb711a62e5c5e63cc16ec0e0f7d1268cc73
Oliver Klee <github@oliverklee.de>
2024-07-04 09:26:54 +0200

    [TASK] Make all PHP files strict

--- src/Property/KeyframeSelector.php
+++ src/Property/KeyframeSelector.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Sabberworm\CSS\Property;
 
 class KeyframeSelector extends Selector
--- tests/RuleSet/LenientParsingTest.php
+++ tests/RuleSet/LenientParsingTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Sabberworm\CSS\Tests\RuleSet;
 
 use PHPUnit\Framework\TestCase;
