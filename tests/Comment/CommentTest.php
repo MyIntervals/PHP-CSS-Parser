60a0a0fd086ab6a3e7bff91e3d12f6cd1903062b
Oliver Klee <github@oliverklee.de>
2024-08-25 14:26:22 +0200

    [TASK] Avoid Hungarian notation in a testcase

--- tests/Comment/CommentTest.php
+++ tests/Comment/CommentTest.php
@@ -154,7 +154,7 @@ final class CommentTest extends TestCase
      */
     public function stripCommentsFromOutput(): void
     {
-        $oCss = TestsParserTest::parsedStructureForFile('comments');
+        $css = TestsParserTest::parsedStructureForFile('comments');
         self::assertSame('
 @import url("some/url.css") screen;
 
@@ -167,12 +167,12 @@ final class CommentTest extends TestCase
 		position: absolute;
 	}
 }
-', $oCss->render(OutputFormat::createPretty()->setRenderComments(false)));
+', $css->render(OutputFormat::createPretty()->setRenderComments(false)));
         self::assertSame(
             '@import url("some/url.css") screen;'
             . '.foo,#bar{background-color:#000;}'
             . '@media screen{#foo.bar{position:absolute;}}',
-            $oCss->render(OutputFormat::createCompact())
+            $css->render(OutputFormat::createCompact())
         );
     }
 }
