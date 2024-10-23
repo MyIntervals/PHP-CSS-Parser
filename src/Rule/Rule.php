MyIntervals/PHP-CSS-Parser

commit e4c66f62c5df3f53f48d82cb30b20b3007af7574

[BUGFIX]  Fix comment parsing to support multiple comments (#672)
Because of an eager consumption of whitespace, the rule parsing would
swallow a trailing comment, meaning the comment for the next rule would
be affected. This patch addresses this by only consuming real whitespace
without comments after a rule.

Fixes #173

Signed-off-by: Daniel Ziegenberg <daniel@ziegenberg.at>
Co-authored-by: Daniel Ziegenberg <daniel@ziegenberg.at>

src/Rule/Rule.php
@@ -105,7 +105,10 @@ class Rule implements Renderable, Commentable
         while ($oParserState->comes(';')) {
             $oParserState->consume(';');
         }
-        $oParserState->consumeWhiteSpace();
+
+        while (\preg_match('/\\s/isSu', $oParserState->peek()) === 1) {
+            $oParserState->consume(1);
+        }
 
         return $oRule;
     }

tests/ParserTest.php
@@ -1159,7 +1159,7 @@ body {background-color: red;}';
     /**
      * @test
      */
-    public function flatCommentExtracting(): void
+    public function flatCommentExtractingOneComment(): void
     {
         $parser = new Parser('div {/*Find Me!*/left:10px; text-align:left;}');
         $doc = $parser->parse();
@@ -1168,6 +1168,22 @@ body {background-color: red;}';
         $comments = $divRules[0]->getComments();
         self::assertCount(1, $comments);
         self::assertSame('Find Me!', $comments[0]->getComment());
+    }
+    /**
+     * @test
+     */
+    public function flatCommentExtractingTwoComments(): void
+    {
+        $parser = new Parser('div {/*Find Me!*/left:10px; /*Find Me Too!*/text-align:left;}');
+        $doc = $parser->parse();
+        $contents = $doc->getContents();
+        $divRules = $contents[0]->getRules();
+        $rule1Comments = $divRules[0]->getComments();
+        $rule2Comments = $divRules[1]->getComments();
+        self::assertCount(1, $rule1Comments);
+        self::assertCount(1, $rule2Comments);
+        self::assertEquals('Find Me!', $rule1Comments[0]->getComment());
+        self::assertEquals('Find Me Too!', $rule2Comments[0]->getComment());
     }
 
     /**

commit a1a9a614cb396922ca4a7a63f633aa1463c84e86

[BUGFIX] Revert broken support for multiple comments
This reverts e4c66f62c5df3f53f48d82cb30b20b3007af7574 (#672), which
broke comment parsing in strict mode.

We'll need to re-implement support for multiple comments later
in a way that does not break the existing comment parsing.

src/Rule/Rule.php
@@ -108,9 +108,7 @@ class Rule implements Renderable, Commentable
             $oParserState->consume(';');
         }
 
-        while (\preg_match('/\\s/isSu', $oParserState->peek()) === 1) {
-            $oParserState->consume(1);
-        }
+        $oParserState->consumeWhiteSpace();
 
         return $oRule;
     }

tests/ParserTest.php
@@ -1171,22 +1171,6 @@ body {background-color: red;}';
         self::assertCount(1, $comments);
         self::assertSame('Find Me!', $comments[0]->getComment());
     }
-    /**
-     * @test
-     */
-    public function flatCommentExtractingTwoComments(): void
-    {
-        $parser = new Parser('div {/*Find Me!*/left:10px; /*Find Me Too!*/text-align:left;}');
-        $document = $parser->parse();
-        $contents = $document->getContents();
-        $divRules = $contents[0]->getRules();
-        $rule1Comments = $divRules[0]->getComments();
-        $rule2Comments = $divRules[1]->getComments();
-        self::assertCount(1, $rule1Comments);
-        self::assertCount(1, $rule2Comments);
-        self::assertEquals('Find Me!', $rule1Comments[0]->getComment());
-        self::assertEquals('Find Me Too!', $rule2Comments[0]->getComment());
-    }
 
     /**
      * @test

tests/RuleSet/DeclarationBlockTest.php
@@ -467,8 +467,6 @@ final class DeclarationBlockTest extends TestCase
         string $cssWithComments,
         string $cssWithoutComments
     ): void {
-        self::markTestSkipped('This currently crashes, and we need to fix it.');
-
         $parserSettings = ParserSettings::create()->withLenientParsing(false);
         $document = (new Parser($cssWithComments, $parserSettings))->parse();
 