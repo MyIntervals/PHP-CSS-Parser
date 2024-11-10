19d58f3bb85396f0c5aa46bfc6c0ed1c8ed4f0b3
Oliver Klee <github@oliverklee.de>
2024-11-10 19:43:59 +0100

    [TASK] Add more Rector rules

--- config/rector.php
+++ config/rector.php
@@ -3,6 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
     ->withPaths(
@@ -11,9 +15,27 @@ return RectorConfig::configure()
             __DIR__ . '/../tests',
         ]
     )
-    ->withPhpSets()
-    ->withRules(
-        [
-            // AddVoidReturnTypeWhereNoReturnRector::class,
-        ]
-    );
+    ->withSets([
+        // Rector sets
+
+        LevelSetList::UP_TO_PHP_73,
+
+        // SetList::CODE_QUALITY,
+        // SetList::CODING_STYLE,
+        // SetList::DEAD_CODE,
+        // SetList::EARLY_RETURN,
+        // SetList::INSTANCEOF,
+        // SetList::NAMING,
+        // SetList::PRIVATIZATION,
+        SetList::STRICT_BOOLEANS,
+        SetList::TYPE_DECLARATION,
+
+        // PHPUnit sets
+
+        PHPUnitSetList::PHPUNIT_80,
+        // PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+    ])
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ])
+    ->withImportNames(true, true, false);
