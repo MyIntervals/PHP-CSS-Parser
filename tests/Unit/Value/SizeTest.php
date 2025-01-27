1f22f330ffa4d6395bb64f0a0e469c9c98941b09
Oliver Klee <github@oliverklee.de>
2025-01-27 21:53:35 +0100

    [TASK] Move `SizeTest` to unit tests
    Also make an anonymous function in the test static.
    
    Part of #758.

--- tests/Unit/Value/SizeTest.php
+++ tests/Unit/Value/SizeTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Sabberworm\CSS\Tests\Value;
+namespace Sabberworm\CSS\Tests\Unit\Value;
 
 use PHPUnit\Framework\TestCase;
 use Sabberworm\CSS\Parsing\ParserState;
@@ -53,7 +53,7 @@ final class SizeTest extends TestCase
         return \array_combine(
             $units,
             \array_map(
-                function (string $unit): array {
+                static function (string $unit): array {
                     return [$unit];
                 },
                 $units
