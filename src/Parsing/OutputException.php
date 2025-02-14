2ed8ad3a904e2691787df8b09eb2ae133d6d9735
Oliver Klee <github@oliverklee.de>
2025-02-14 23:18:51 +0100

    [TASK] Drop redundant `OutputException` constructor

--- src/Parsing/OutputException.php
+++ src/Parsing/OutputException.php
@@ -7,13 +7,4 @@ namespace Sabberworm\CSS\Parsing;
 /**
  * Thrown if the CSS parser attempts to print something invalid.
  */
-final class OutputException extends SourceException
-{
-    /**
-     * @param int<0, max> $lineNumber
-     */
-    public function __construct(string $sMessage, int $lineNumber = 0)
-    {
-        parent::__construct($sMessage, $lineNumber);
-    }
-}
+final class OutputException extends SourceException {}
