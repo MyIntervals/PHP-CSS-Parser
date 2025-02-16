96828776b2a3881858ff9d978d511aa47ab953e1
Oliver Klee <github@oliverklee.de>
2025-02-16 10:36:39 +0100

    [TASK] Add some more unit tests for `AtRuleBlockList` (#934)
    Part of #757

--- tests/Unit/CSSList/AtRuleBlockListTest.php
+++ tests/Unit/CSSList/AtRuleBlockListTest.php
@@ -11,6 +11,8 @@ use Sabberworm\CSS\Renderable;
 
 /**
  * @covers \Sabberworm\CSS\CSSList\AtRuleBlockList
+ * @covers \Sabberworm\CSS\CSSList\CSSBlockList
+ * @covers \Sabberworm\CSS\CSSList\CSSList
  */
 final class AtRuleBlockListTest extends TestCase
 {
@@ -42,5 +44,71 @@ final class AtRuleBlockListTest extends TestCase
         $subject = new AtRuleBlockList('');
 
         self::assertInstanceOf(Commentable::class, $subject);
+    }
+
+    /**
+     * @test
+     */
+    public function atRuleNameReturnsTypeProvidedToConstructor(): void
+    {
+        $type = 'foo';
+
+        $subject = new AtRuleBlockList($type);
+
+        self::assertSame($type, $subject->atRuleName());
+    }
+
+    /**
+     * @test
+     */
+    public function getLineNoByDefaultReturnsZero(): void
+    {
+        $subject = new AtRuleBlockList('');
+
+        self::assertSame(0, $subject->getLineNo());
+    }
+
+    /**
+     * @test
+     */
+    public function atRuleArgsByDefaultReturnsEmptyString(): void
+    {
+        $subject = new AtRuleBlockList('');
+
+        self::assertSame('', $subject->atRuleArgs());
+    }
+
+    /**
+     * @test
+     */
+    public function atRuleArgsReturnsArgumentsProvidedToConstructor(): void
+    {
+        $arguments = 'bar';
+
+        $subject = new AtRuleBlockList('', $arguments);
+
+        self::assertSame($arguments, $subject->atRuleArgs());
+    }
+
+    /**
+     * @test
+     */
+    public function getLineNoReturnsLineNumberProvidedToConstructor(): void
+    {
+        $lineNumber = 42;
+
+        $subject = new AtRuleBlockList('', '', $lineNumber);
+
+        self::assertSame($lineNumber, $subject->getLineNo());
+    }
+
+    /**
+     * @test
+     */
+    public function isRootListAlwaysReturnsFalse(): void
+    {
+        $subject = new AtRuleBlockList('');
+
+        self::assertFalse($subject->isRootList());
     }
 }
