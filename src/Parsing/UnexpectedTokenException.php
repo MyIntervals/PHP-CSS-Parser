3be7c0ab2441012b25bcbad554b36278acb03bf3
Oliver Klee <github@oliverklee.de>
2025-02-24 14:47:25 +0100

    [TASK] Drop unnecessary properties from `UnexpectedTokenException`

--- src/Parsing/UnexpectedTokenException.php
+++ src/Parsing/UnexpectedTokenException.php
@@ -10,37 +10,19 @@ namespace Sabberworm\CSS\Parsing;
 class UnexpectedTokenException extends SourceException
 {
     /**
-     * @var string
-     */
-    private $expected;
-
-    /**
-     * @var string
-     */
-    private $found;
-
-    /**
-     * @var 'literal'|'identifier'|'count'|'expression'|'search'|'custom'
-     */
-    private $matchType;
-
-    /**
      * @param 'literal'|'identifier'|'count'|'expression'|'search'|'custom' $matchType
      * @param int<0, max> $lineNumber
      */
     public function __construct(string $expected, string $found, string $matchType = 'literal', int $lineNumber = 0)
     {
-        $this->expected = $expected;
-        $this->found = $found;
-        $this->matchType = $matchType;
         $message = "Token “{$expected}” ({$matchType}) not found. Got “{$found}”.";
-        if ($this->matchType === 'search') {
+        if ($matchType === 'search') {
             $message = "Search for “{$expected}” returned no results. Context: “{$found}”.";
-        } elseif ($this->matchType === 'count') {
+        } elseif ($matchType === 'count') {
             $message = "Next token was expected to have {$expected} chars. Context: “{$found}”.";
-        } elseif ($this->matchType === 'identifier') {
+        } elseif ($matchType === 'identifier') {
             $message = "Identifier expected. Got “{$found}”";
-        } elseif ($this->matchType === 'custom') {
+        } elseif ($matchType === 'custom') {
             $message = \trim("$expected $found");
         }
 
