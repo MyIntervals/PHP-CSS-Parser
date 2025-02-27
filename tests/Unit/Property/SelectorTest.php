b271be27c408e101be73deeeeda4e3683d408c84
Oliver Klee <github@oliverklee.de>
2025-02-27 13:27:00 +0100

    [TASK] Add tests for the selector specificity
    Mostly move tests from `ParserTest` that belong into
    the unit tests.
    
    Part of #757
    Part of #758

--- tests/Unit/Property/SelectorTest.php
+++ tests/Unit/Property/SelectorTest.php
@@ -35,4 +35,56 @@ final class SelectorTest extends TestCase
 
         self::assertSame($selector, $subject->getSelector());
     }
+
+    /**
+     * @return array<string, array{0: non-empty-string, 1: int<0, max>}>
+     */
+    public static function provideSelectorsAndSpecificities(): array
+    {
+        return [
+            'element' => ['a', 1],
+            'element and descendant with pseudo-selector' => ['ol li::before', 3],
+            'class' => ['.highlighted', 10],
+            'element with class' => ['li.green', 11],
+            'class with pseudo-selector' => ['.help:hover', 20],
+            'ID' => ['#file', 100],
+            'ID and descendant class' => ['#test .help', 110],
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * @param non-empty-string $selector
+     * @param int<0, max> $expectedSpecificity
+     *
+     * @dataProvider provideSelectorsAndSpecificities
+     */
+    public function getSpecificityByDefaultReturnsSpecificityOfSelectorProvidedToConstructor(
+        string $selector,
+        int $expectedSpecificity
+    ): void {
+        $subject = new Selector($selector);
+
+        self::assertSame($expectedSpecificity, $subject->getSpecificity());
+    }
+
+    /**
+     * @test
+     *
+     * @param non-empty-string $selector
+     * @param int<0, max> $expectedSpecificity
+     *
+     * @dataProvider provideSelectorsAndSpecificities
+     */
+    public function getSpecificityByReturnsSpecificityOfSelectorLastProvidedViaSetSelector(
+        string $selector,
+        int $expectedSpecificity
+    ): void {
+        $subject = new Selector('p');
+
+        $subject->setSelector($selector);
+
+        self::assertSame($expectedSpecificity, $subject->getSpecificity());
+    }
 }
