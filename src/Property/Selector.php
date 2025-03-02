8570e6b385737280dba26cc4138ffb71fd6cd462
Oliver Klee <github@oliverklee.de>
2025-03-02 20:36:42 +0100

    Use the new utility class

--- src/Property/Selector.php
+++ src/Property/Selector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Sabberworm\CSS\Property;
 
 use Sabberworm\CSS\OutputFormat;
+use Sabberworm\CSS\Property\Selector\SpecificityCalculator;
 use Sabberworm\CSS\Renderable;
 
 /**
@@ -13,43 +14,6 @@ use Sabberworm\CSS\Renderable;
  */
 class Selector implements Renderable
 {
-    /**
-     * regexp for specificity calculations
-     *
-     * @var string
-     */
-    private const NON_ID_ATTRIBUTES_AND_PSEUDO_CLASSES_RX = '/
-        (\\.[\\w]+)                   # classes
-        |
-        \\[(\\w+)                     # attributes
-        |
-        (\\:(                         # pseudo classes
-            link|visited|active
-            |hover|focus
-            |lang
-            |target
-            |enabled|disabled|checked|indeterminate
-            |root
-            |nth-child|nth-last-child|nth-of-type|nth-last-of-type
-            |first-child|last-child|first-of-type|last-of-type
-            |only-child|only-of-type
-            |empty|contains
-        ))
-        /ix';
-
-    /**
-     * regexp for specificity calculations
-     *
-     * @var string
-     */
-    private const ELEMENTS_AND_PSEUDO_ELEMENTS_RX = '/
-        ((^|[\\s\\+\\>\\~]+)[\\w]+   # elements
-        |
-        \\:{1,2}(                    # pseudo-elements
-            after|before|first-letter|first-line|selection
-        ))
-        /ix';
-
     /**
      * regexp for specificity calculations
      *
@@ -110,14 +74,7 @@ class Selector implements Renderable
      */
     public function getSpecificity(): int
     {
-        $a = 0;
-        // @todo should exclude \# as well as "#"
-        $aMatches = null;
-        $b = \substr_count($this->selector, '#');
-        $c = \preg_match_all(self::NON_ID_ATTRIBUTES_AND_PSEUDO_CLASSES_RX, $this->selector, $aMatches);
-        $d = \preg_match_all(self::ELEMENTS_AND_PSEUDO_ELEMENTS_RX, $this->selector, $aMatches);
-
-        return ($a * 1000) + ($b * 100) + ($c * 10) + $d;
+        return SpecificityCalculator::calculate($this->selector);
     }
 
     public function render(OutputFormat $outputFormat): string
