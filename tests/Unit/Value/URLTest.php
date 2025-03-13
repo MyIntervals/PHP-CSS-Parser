6a09851fae6b97810b5319627b90ce6d88766b20
Oliver Klee <github@oliverklee.de>
2025-03-13 18:14:38 +0100

    [TASK] Add some basic unit tests for `URL` (#1164)
    Part of #757
    
    Co-authored-by: JakeQZ <jake.github@qzdesign.co.uk>

---
+++ tests/Unit/Value/URLTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sabberworm\CSS\Tests\Unit\Value;
+
+use PHPUnit\Framework\TestCase;
+use Sabberworm\CSS\Value\CSSString;
+use Sabberworm\CSS\Value\PrimitiveValue;
+use Sabberworm\CSS\Value\URL;
+use Sabberworm\CSS\Value\Value;
+
+/**
+ * @covers \Sabberworm\CSS\Value\PrimitiveValue
+ * @covers \Sabberworm\CSS\Value\URL
+ * @covers \Sabberworm\CSS\Value\Value
+ */
+final class URLTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function isPrimitiveValue(): void
+    {
+        $subject = new URL(new CSSString('http://example.com'));
+
+        self::assertInstanceOf(PrimitiveValue::class, $subject);
+    }
+
+    /**
+     * @test
+     */
+    public function isValue(): void
+    {
+        $subject = new URL(new CSSString('http://example.com'));
+
+        self::assertInstanceOf(Value::class, $subject);
+    }
+
+    /**
+     * @test
+     */
+    public function getUrlByDefaultReturnsUrlProvidedToConstructor(): void
+    {
+        $subject = new CSSString('http://example.com');
+
+        self::assertSame($subject, (new URL($subject))->getURL());
+    }
+
+    /**
+     * @test
+     */
+    public function setUrlReplacesUrl(): void
+    {
+        $subject = new URL(new CSSString('http://example.com'));
+
+        $newUrl = new CSSString('http://example.org');
+        $subject->setURL($newUrl);
+
+        self::assertSame($newUrl, $subject->getURL());
+    }
+
+    /**
+     * @test
+     */
+    public function getLineNoByDefaultReturnsZero(): void
+    {
+        $subject = new URL(new CSSString('http://example.com'));
+
+        self::assertSame(0, $subject->getLineNo());
+    }
+
+    /**
+     * @test
+     */
+    public function getLineNoReturnsLineNumberProvidedToConstructor(): void
+    {
+        $lineNumber = 17;
+
+        $subject = new URL(new CSSString('http://example.com'), $lineNumber);
+
+        self::assertSame($lineNumber, $subject->getLineNo());
+    }
+}
