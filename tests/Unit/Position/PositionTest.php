f54dadbf08c68efd108440d818391f4e85f715ed
Jake Hotson <jake.github@qzdesign.co.uk>
2025-03-28 02:24:40 +0000

    [TASK] Add `Positionable` interface and implementing trait
    This is for CSS items which have a position in the document.
    
    New methods are added:
    - `getLineNumber` to replace `getLineNo`;
    - `getColumnNumber` to replace `getColNo`.
    
    These return a nullable `int`, instead of using zero to indicate absence.
    
    The old methods are now deprecated,
    but defined in the interface and implemented in the trait.
    
    Note that this change only adds the interface and trait.
    It does not modify any classes to actually implement or use these.
    
    Part of #1207.

---
+++ tests/Unit/Position/PositionTest.php
@@ -0,0 +1,284 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sabberworm\CSS\Tests\Unit\Position;
+
+use PHPUnit\Framework\TestCase;
+use Sabberworm\CSS\Tests\Unit\Position\Fixtures\ConcretePosition;
+use TRegx\DataProvider\DataProviders;
+
+/**
+ * @covers \Sabberworm\CSS\Position\Position
+ */
+final class PositionTest extends TestCase
+{
+    /**
+     * @var ConcretePosition
+     */
+    private $subject;
+
+    protected function setUp(): void
+    {
+        $this->subject = new ConcretePosition();
+    }
+
+    /**
+     * @test
+     */
+    public function getLineNumberInitiallyReturnsNull(): void
+    {
+        self::assertNull($this->subject->getLineNumber());
+    }
+
+    /**
+     * @test
+     */
+    public function getColumnNumberInitiallyReturnsNull(): void
+    {
+        self::assertNull($this->subject->getColumnNumber());
+    }
+
+    /**
+     * @return array<non-empty-string, array{0: int<1, max>}>
+     */
+    public function provideLineNumber(): array
+    {
+        return [
+            'line 1' => [1],
+            'line 42' => [42],
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * @param int<1, max> $lineNumber
+     *
+     * @dataProvider provideLineNumber
+     */
+    public function setPositionOnVirginSetsLineNumber(int $lineNumber): void
+    {
+        $this->subject->setPosition($lineNumber);
+
+        self::assertSame($lineNumber, $this->subject->getLineNumber());
+    }
+
+    /**
+     * @test
+     *
+     * @param int<1, max> $lineNumber
+     *
+     * @dataProvider provideLineNumber
+     */
+    public function setPositionSetsNewLineNumber(int $lineNumber): void
+    {
+        $this->subject->setPosition(99);
+
+        $this->subject->setPosition($lineNumber);
+
+        self::assertSame($lineNumber, $this->subject->getLineNumber());
+    }
+
+    /**
+     * @test
+     */
+    public function setPositionWithNullClearsLineNumber(): void
+    {
+        $this->subject->setPosition(99);
+
+        $this->subject->setPosition(null);
+
+        self::assertNull($this->subject->getLineNumber());
+    }
+
+    /**
+     * @return array<non-empty-string, array{0: int<0, max>}>
+     */
+    public function provideColumnNumber(): array
+    {
+        return [
+            'column 0' => [0],
+            'column 14' => [14],
+            'column 39' => [39],
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * @param int<0, max> $columnNumber
+     *
+     * @dataProvider provideColumnNumber
+     */
+    public function setPositionOnVirginSetsColumnNumber(int $columnNumber): void
+    {
+        $this->subject->setPosition(1, $columnNumber);
+
+        self::assertSame($columnNumber, $this->subject->getColumnNumber());
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider provideColumnNumber
+     */
+    public function setPositionSetsNewColumnNumber(int $columnNumber): void
+    {
+        $this->subject->setPosition(1, 99);
+
+        $this->subject->setPosition(2, $columnNumber);
+
+        self::assertSame($columnNumber, $this->subject->getColumnNumber());
+    }
+
+    /**
+     * @test
+     */
+    public function setPositionWithoutColumnNumberClearsColumnNumber(): void
+    {
+        $this->subject->setPosition(1, 99);
+
+        $this->subject->setPosition(2);
+
+        self::assertNull($this->subject->getColumnNumber());
+    }
+
+    /**
+     * @test
+     */
+    public function setPositionWithNullForColumnNumberClearsColumnNumber(): void
+    {
+        $this->subject->setPosition(1, 99);
+
+        $this->subject->setPosition(2, null);
+
+        self::assertNull($this->subject->getColumnNumber());
+    }
+
+    /**
+     * @return array<non-empty-string, array{0: int<1, max>, 1: int<0, max>}>
+     */
+    public function provideLineAndColumnNumber(): array
+    {
+        return DataProviders::cross($this->provideLineNumber(), $this->provideColumnNumber());
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider provideLineAndColumnNumber
+     */
+    public function setPositionOnVirginSetsLineAndColumnNumber(int $lineNumber, int $columnNumber): void
+    {
+        $this->subject->setPosition($lineNumber, $columnNumber);
+
+        self::assertSame($lineNumber, $this->subject->getLineNumber());
+        self::assertSame($columnNumber, $this->subject->getColumnNumber());
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider provideLineAndColumnNumber
+     */
+    public function setPositionSetsNewLineAndColumnNumber(int $lineNumber, int $columnNumber): void
+    {
+        $this->subject->setPosition(98, 99);
+
+        $this->subject->setPosition($lineNumber, $columnNumber);
+
+        self::assertSame($lineNumber, $this->subject->getLineNumber());
+        self::assertSame($columnNumber, $this->subject->getColumnNumber());
+    }
+
+    // Testing deprecated functionality
+
+    /**
+     * @test
+     */
+    public function getLineNoInitiallyReturnsZero(): void
+    {
+        self::assertSame(0, $this->subject->getLineNo());
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider provideLineNumber
+     */
+    public function getLineNoReturnsLineNumberSet(int $lineNumber): void
+    {
+        $this->subject->setPosition($lineNumber);
+
+        self::assertSame($lineNumber, $this->subject->getLineNo());
+    }
+
+    /**
+     * @test
+     */
+    public function getLineNoReturnsZeroAfterLineNumberCleared(): void
+    {
+        $this->subject->setPosition(99);
+
+        $this->subject->setPosition(null);
+
+        self::assertSame(0, $this->subject->getLineNo());
+    }
+
+    /**
+     * @test
+     */
+    public function getColNoInitiallyReturnsZero(): void
+    {
+        self::assertSame(0, $this->subject->getColNo());
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider provideColumnNumber
+     */
+    public function getColNoReturnsColumnNumberSet(int $columnNumber): void
+    {
+        $this->subject->setPosition(1, $columnNumber);
+
+        self::assertSame($columnNumber, $this->subject->getColNo());
+    }
+
+    /**
+     * @test
+     */
+    public function getColNoReturnsZeroAfterColumnNumberCleared(): void
+    {
+        $this->subject->setPosition(1, 99);
+
+        $this->subject->setPosition(2);
+
+        self::assertSame(0, $this->subject->getColNo());
+    }
+
+    /**
+     * @test
+     */
+    public function setPositionWithZeroClearsLineNumber(): void
+    {
+        $this->subject->setPosition(99);
+
+        $this->subject->setPosition(0);
+
+        self::assertNull($this->subject->getLineNumber());
+    }
+
+    /**
+     * @test
+     */
+    public function getLineNoAfterSetPositionWithZeroReturnsZero(): void
+    {
+        $this->subject->setPosition(99);
+
+        $this->subject->setPosition(0);
+
+        self::assertSame(0, $this->subject->getLineNo());
+    }
+}
