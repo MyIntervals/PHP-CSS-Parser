e06ed2eeff3d237a7be05ba0edac0e69c22ecc59
JakeQZ <jake.github@qzdesign.co.uk>
2025-04-07 10:22:26 +0100

    Apply #1233 (and #1235)
    Also include the relevant DocBlock reordering from autoformatting in #1228.

--- src/Position/Positionable.php
+++ src/Position/Positionable.php
@@ -17,9 +17,9 @@ interface Positionable
     public function getLineNumber();
 
     /**
-     * @deprecated in version 9.0.0, will be removed in v10.0. Use `getLineNumber()` instead.
-     *
      * @return int<0, max>
+     *
+     * @deprecated in version 8.9.0, will be removed in v9.0. Use `getLineNumber()` instead.
      */
     public function getLineNo();
 
@@ -29,15 +29,15 @@ interface Positionable
     public function getColumnNumber();
 
     /**
-     * @deprecated in version 9.0.0, will be removed in v10.0. Use `getColumnNumber()` instead.
-     *
      * @return int<0, max>
+     *
+     * @deprecated in version 8.9.0, will be removed in v9.0. Use `getColumnNumber()` instead.
      */
     public function getColNo();
 
     /**
      * @param int<0, max>|null $lineNumber
-     *        Providing zero for this parameter is deprecated in version 9.0.0, and will not be supported from v10.0.
+     *        Providing zero for this parameter is deprecated in version 8.9.0, and will not be supported from v9.0.
      *        Use `null` instead when no line number is available.
      * @param int<0, max>|null $columnNumber
      */
