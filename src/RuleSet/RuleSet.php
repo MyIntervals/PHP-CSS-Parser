fdbb925ca0bc945b1cd246e8a6e47cc4a33d344e
JakeQZ <jake.github@qzdesign.co.uk>
2025-06-05 15:17:41 +0100

    [BUGFIX] `AddRule` before sibling with different property name (#1270)
    Part of #974.

--- src/RuleSet/RuleSet.php
+++ src/RuleSet/RuleSet.php
@@ -104,9 +104,25 @@ abstract class RuleSet implements CSSElement, CSSListItem, Positionable, RuleCon
         $position = \count($this->rules[$propertyName]);
 
         if ($sibling !== null) {
+            $siblingIsInSet = false;
             $siblingPosition = \array_search($sibling, $this->rules[$propertyName], true);
             if ($siblingPosition !== false) {
+                $siblingIsInSet = true;
                 $position = $siblingPosition;
+            } else {
+                $siblingIsInSet = $this->hasRule($sibling);
+                if ($siblingIsInSet) {
+                    // Maintain ordering within `$this->rules[$propertyName]`
+                    // by inserting before first `Rule` with a same-or-later position than the sibling.
+                    foreach ($this->rules[$propertyName] as $index => $rule) {
+                        if (self::comparePositionable($rule, $sibling) >= 0) {
+                            $position = $index;
+                            break;
+                        }
+                    }
+                }
+            }
+            if ($siblingIsInSet) {
                 // Increment column number of all existing rules on same line, starting at sibling
                 $siblingLineNumber = $sibling->getLineNumber();
                 $siblingColumnNumber = $sibling->getColumnNumber();
@@ -123,6 +139,7 @@ abstract class RuleSet implements CSSElement, CSSListItem, Positionable, RuleCon
                 $ruleToAdd->setPosition($siblingLineNumber, $siblingColumnNumber);
             }
         }
+
         if ($ruleToAdd->getLineNumber() === null) {
             //this node is added manually, give it the next best line
             $columnNumber = $ruleToAdd->getColumnNumber() ?? 0;
@@ -304,5 +321,16 @@ abstract class RuleSet implements CSSElement, CSSListItem, Positionable, RuleCon
             return $first->getColNo() - $second->getColNo();
         }
         return $first->getLineNo() - $second->getLineNo();
+    }
+
+    private function hasRule(Rule $rule): bool
+    {
+        foreach ($this->rules as $rulesForAProperty) {
+            if (\in_array($rule, $rulesForAProperty, true)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
