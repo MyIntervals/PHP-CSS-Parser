MyIntervals/PHP-CSS-Parser

commit f9685bb4406686d7657952aa84e719040d2a1fb3

[TASK] Set line number to `null` by default (#1288)
No longer allow or support `0` as a default line or column number.

Part of #974

src/CSSList/AtRuleBlockList.php
@@ -24,9 +24,9 @@ class AtRuleBlockList extends CSSBlockList implements AtRule
 
     /**
      * @param non-empty-string $type
-     * @param int<0, max> $lineNumber
+     * @param int<1, max>|null $lineNumber
      */
-    public function __construct(string $type, string $arguments = '', int $lineNumber = 0)
+    public function __construct(string $type, string $arguments = '', ?int $lineNumber = null)
     {
         parent::__construct($lineNumber);
         $this->type = $type;

src/CSSList/CSSList.php
@@ -48,9 +48,9 @@ abstract class CSSList implements CSSElement, CSSListItem, Positionable
     protected $contents = [];
 
     /**
-     * @param int<0, max> $lineNumber
+     * @param int<1, max>|null $lineNumber
      */
-    public function __construct(int $lineNumber = 0)
+    public function __construct(?int $lineNumber = null)
     {
         $this->setPosition($lineNumber);
     }

src/Comment/Comment.php
@@ -21,9 +21,9 @@ class Comment implements Positionable, Renderable
     protected $commentText;
 
     /**
-     * @param int<0, max> $lineNumber
+     * @param int<1, max>|null $lineNumber
      */
-    public function __construct(string $commentText = '', int $lineNumber = 0)
+    public function __construct(string $commentText = '', ?int $lineNumber = null)
     {
         $this->commentText = $commentText;
         $this->setPosition($lineNumber);

src/Parsing/SourceException.php
@@ -12,9 +12,9 @@ class SourceException extends \Exception implements Positionable
     use Position;
 
     /**
-     * @param int<0, max> $lineNumber
+     * @param int<1, max>|null $lineNumber
      */
-    public function __construct(string $message, int $lineNumber = 0)
+    public function __construct(string $message, ?int $lineNumber = null)
     {
         $this->setPosition($lineNumber);
         if ($lineNumber !== 0) {

src/Parsing/UnexpectedTokenException.php
@@ -11,9 +11,9 @@ class UnexpectedTokenException extends SourceException
 {
     /**
      * @param 'literal'|'identifier'|'count'|'expression'|'search'|'custom' $matchType
-     * @param int<0, max> $lineNumber
+     * @param int<1, max>|null $lineNumber
      */
-    public function __construct(string $expected, string $found, string $matchType = 'literal', int $lineNumber = 0)
+    public function __construct(string $expected, string $found, string $matchType = 'literal', ?int $lineNumber = null)
     {
         $message = "Token “{$expected}” ({$matchType}) not found. Got “{$found}”.";
         if ($matchType === 'search') {

src/Position/Position.php
@@ -48,15 +48,14 @@ trait Position
     }
 
     /**
-     * @param int<0, max>|null $lineNumber
+     * @param int<1, max>|null $lineNumber
      * @param int<0, max>|null $columnNumber
      *
      * @return $this fluent interface
      */
     public function setPosition(?int $lineNumber, ?int $columnNumber = null): Positionable
     {
-        // The conditional is for backwards compatibility (backcompat); `0` will not be allowed in future.
-        $this->lineNumber = $lineNumber !== 0 ? $lineNumber : null;
+        $this->lineNumber = $lineNumber;
         $this->columnNumber = $columnNumber;
 
         return $this;

src/Position/Positionable.php
@@ -29,9 +29,7 @@ interface Positionable
     public function getColumnNumber(): ?int;
 
     /**
-     * @param int<0, max>|null $lineNumber
-     *        Providing zero for this parameter is deprecated in version 8.9.0, and will not be supported from v9.0.
-     *        Use `null` instead when no line number is available.
+     * @param int<1, max>|null $lineNumber
      * @param int<0, max>|null $columnNumber
      *
      * @return $this fluent interface

src/Property/CSSNamespace.php
@@ -31,9 +31,9 @@ class CSSNamespace implements AtRule, Positionable
 
     /**
      * @param CSSString|URL $url
-     * @param int<0, max> $lineNumber
+     * @param int<1, max>|null $lineNumber
      */
-    public function __construct($url, ?string $prefix = null, int $lineNumber = 0)
+    public function __construct($url, ?string $prefix = null, ?int $lineNumber = null)
     {
         $this->url = $url;
         $this->prefix = $prefix;

commit c4c0db8810e3e2414f34fb1dfe6bd463c0ffe5cd

[BUGFIX] Exclude absent line number from exception message
The bug was introduced by #1288, so has not been included in any release;
thus a changelog entry is not justified.

tests/Unit/Parsing/SourceExceptionTest.php
@@ -20,7 +20,7 @@ final class SourceExceptionTest extends TestCase
         $message = 'The cake is a lie.';
         $exception = new SourceException($message);
 
-        self::assertStringContainsString($message, $exception->getMessage());
+        self::assertSame($message, $exception->getMessage());
     }
 
     /**
@@ -53,6 +53,17 @@ final class SourceExceptionTest extends TestCase
         $exception = new SourceException('foo', $lineNumber);
 
         self::assertStringContainsString(' [line no: ' . $lineNumber . ']', $exception->getMessage());
+    }
+
+    /**
+     * @test
+     */
+    public function getMessageWithLineNumberProvidedIncludesMessage(): void
+    {
+        $message = 'There is no flatware.';
+        $exception = new SourceException($message, 17);
+
+        self::assertStringContainsString($message, $exception->getMessage());
     }
 
     /**