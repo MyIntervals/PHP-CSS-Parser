b1b8a8f616367900c632b43750781954173e4f47
Jake Hotson <jake.github@qzdesign.co.uk>
2025-06-27 15:20:43 +0100

    Add tests for `getRuleSet`

--- tests/Unit/RuleSet/DeclarationBlockTest.php
+++ tests/Unit/RuleSet/DeclarationBlockTest.php
@@ -8,7 +8,9 @@ use PHPUnit\Framework\TestCase;
 use Sabberworm\CSS\CSSElement;
 use Sabberworm\CSS\CSSList\CSSListItem;
 use Sabberworm\CSS\Position\Positionable;
+use Sabberworm\CSS\Rule\Rule;
 use Sabberworm\CSS\RuleSet\DeclarationBlock;
+use Sabberworm\CSS\RuleSet\RuleSet;
 
 /**
  * @covers \Sabberworm\CSS\RuleSet\DeclarationBlock
@@ -49,5 +51,54 @@ final class DeclarationBlockTest extends TestCase
     public function implementsPositionable(): void
     {
         self::assertInstanceOf(Positionable::class, $this->subject);
+    }
+
+    /**
+     * @test
+     */
+    public function getRuleSetOnVirginReturnsARuleSet(): void
+    {
+        $result = $this->subject->getRuleSet();
+
+        self::assertInstanceOf(RuleSet::class, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function getRuleSetAfterRulesSetReturnsARuleSet(): void
+    {
+        $this->subject->setRules([new Rule('color')]);
+
+        $result = $this->subject->getRuleSet();
+
+        self::assertInstanceOf(RuleSet::class, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function getRuleSetOnVirginReturnsObjectWithoutRules(): void
+    {
+        $result = $this->subject->getRuleSet();
+
+        self::assertSame([], $result->getRules());
+    }
+
+    /**
+     * @test
+     *
+     * @param list<string> $propertyNamesToSet
+     *
+     * @dataProvider providePropertyNames
+     */
+    public function getRuleSetReturnsObjectWithRulesSet(array $propertyNamesToSet): void
+    {
+        $rules = self::createRulesFromPropertyNames($propertyNamesToSet);
+        $this->subject->setRules($rules);
+
+        $result = $this->subject->getRuleSet();
+
+        self::assertSame($rules, $result->getRules());
     }
 }
