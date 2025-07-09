595a47b75d591ab28592722334b0d06625dedd48
JakeQZ <jake.github@qzdesign.co.uk>
2025-07-09 23:26:22 +0100

    [TASK] Add unit tests for selector parsing (#1322)
    The tests broadly cover what currently works, and will be extended to cover
    the fixes in #1292.

--- tests/Unit/RuleSet/DeclarationBlockTest.php
+++ tests/Unit/RuleSet/DeclarationBlockTest.php
@@ -8,7 +8,11 @@ use PHPUnit\Framework\TestCase;
 use Sabberworm\CSS\CSSElement;
 use Sabberworm\CSS\CSSList\CSSListItem;
 use Sabberworm\CSS\Position\Positionable;
+use Sabberworm\CSS\Parsing\ParserState;
+use Sabberworm\CSS\Property\Selector;
 use Sabberworm\CSS\RuleSet\DeclarationBlock;
+use Sabberworm\CSS\Settings;
+use TRegx\PhpUnit\DataProviders\DataProvider;
 
 /**
  * @covers \Sabberworm\CSS\RuleSet\DeclarationBlock
@@ -47,5 +51,79 @@ final class DeclarationBlockTest extends TestCase
     public function implementsPositionable(): void
     {
         self::assertInstanceOf(Positionable::class, $this->subject);
+    }
+
+    /**
+     * @return array<non-empty-string, array{0: non-empty-string}>
+     */
+    public static function provideSelector(): array
+    {
+        return [
+            'type' => ['body'],
+            'class' => ['.teapot'],
+            'type & class' => ['img.teapot'],
+            'id' => ['#my-mug'],
+            'type & id' => ['h2#my-mug'],
+            'pseudo-class' => [':hover'],
+            'type & pseudo-class' => ['a:hover'],
+            '`not`' => [':not(#your-mug)'],
+            'pseudo-element' => ['::before'],
+            'attribute with `"`' => ['[alt="{}()[]\\"\'"]'],
+            'attribute with `\'`' => ['[alt=\'{}()[]"\\\'\']'],
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * @param non-empty-string $selector
+     *
+     * @dataProvider provideSelector
+     */
+    public function parsesSingleSelector(string $selector): void
+    {
+        $subject = DeclarationBlock::parse(new ParserState($selector . ' {}', Settings::create()));
+
+        self::assertInstanceOf(DeclarationBlock::class, $subject);
+        self::assertSame([$selector], self::getSelectorsAsStrings($subject));
+    }
+
+    /**
+     * @return DataProvider<non-empty-string, array{0: non-empty-string, 1: non-empty-string}>
+     */
+    public static function provideTwoSelectors(): DataProvider
+    {
+        return DataProvider::cross(self::provideSelector(), self::provideSelector());
+    }
+
+    /**
+     * @test
+     *
+     * @param non-empty-string $firstSelector
+     * @param non-empty-string $secondSelector
+     *
+     * @dataProvider provideTwoSelectors
+     */
+    public function parsesTwoCommaSeparatedSelectors(string $firstSelector, string $secondSelector): void
+    {
+        $joinedSelectors = $firstSelector . ', ' . $secondSelector;
+
+        $subject = DeclarationBlock::parse(new ParserState($joinedSelectors . ' {}', Settings::create()));
+
+        self::assertInstanceOf(DeclarationBlock::class, $subject);
+        self::assertSame([$firstSelector, $secondSelector], self::getSelectorsAsStrings($subject));
+    }
+
+    /**
+     * @return array<string>
+     */
+    private static function getSelectorsAsStrings(DeclarationBlock $declarationBlock): array
+    {
+        return \array_map(
+            static function (Selector $selectorObject): string {
+                return $selectorObject->getSelector();
+            },
+            $declarationBlock->getSelectors()
+        );
     }
 }
