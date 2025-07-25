d3527f0fff25a0c07fbb592592747ff185e442d1
Oliver Klee <github@oliverklee.de>
2025-07-25 16:43:00 +0200

    WIP

---
+++ tests/Support/TraversableContains.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace Sabberworm\CSS\Tests\Support;
+
+/**
+ * Concrete subclass of the abstract PHPUnit constraint class.
+ */
+final class TraversableContains extends \PHPUnit\Framework\Constraint\TraversableContains
+{
+
+}
