36b495a2a691b955782311045dae9d8bd06bd5e2
Oliver Klee <github@oliverklee.de>
2025-12-04 13:12:14 +0100

    [TASK] Add coverage metadata to `AtRuleTest`
    As interfaces cannot be covered using an `@covers` annotation,
    we need to use `@coversNothing` instead.
    
    This will allow us to require coverage metadata for testcases
    later down the road.

--- tests/Unit/Property/AtRuleTest.php
+++ tests/Unit/Property/AtRuleTest.php
@@ -7,6 +7,9 @@ namespace Sabberworm\CSS\Tests\Unit\Property;
 use PHPUnit\Framework\TestCase;
 use Sabberworm\CSS\Property\AtRule;
 
+/**
+ * @coversNothing
+ */
 final class AtRuleTest extends TestCase
 {
     /**
