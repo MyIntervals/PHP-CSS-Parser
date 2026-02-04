104b94d599e68afb59963a609f1d1eb5688da848
JakeQZ <jake.github@qzdesign.co.uk>
2026-02-04 21:08:37 +0000

    [TASK] Add `Selector\Combinator` class (#1487)

---
+++ tests/Unit/Property/Selector/CombinatorTest.php
@@ -0,0 +1,300 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sabberworm\CSS\Tests\Unit\Property\Selector;
+
+use PHPUnit\Framework\TestCase;
+use Sabberworm\CSS\OutputFormat;
+use Sabberworm\CSS\Parsing\ParserState;
+use Sabberworm\CSS\Parsing\UnexpectedTokenException;
+use Sabberworm\CSS\Property\Selector\Combinator;
+use Sabberworm\CSS\Property\Selector\Component;
+use Sabberworm\CSS\Renderable;
+use Sabberworm\CSS\Settings;
+
+/**
+ * @covers \Sabberworm\CSS\Property\Selector\Combinator
+ *
+ * @phpstan-import-type ValidCombinatorValue from Combinator
+ */
+final class CombinatorTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function implementsRenderable(): void
+    {
+        self::assertInstanceOf(Renderable::class, new Combinator('>'));
+    }
+
+    /**
+     * @test
+     */
+    public function implementsSelectorComponent(): void
+    {
+        self::assertInstanceOf(Component::class, new Combinator('>'));
+    }
+
+    /**
+     * @return array<non-empty-string, array{0: ValidCombinatorValue}>
+     */
+    public static function provideValidValue(): array
+    {
+        return [
+            'descendent' => [' '],
+            'child' => ['>'],
+            'next sibling' => ['+'],
+            'subsequent sibling' => ['~'],
+        ];
+    }
+
+    /**
+     * @return array<non-empty-string, array{0: non-empty-string}>
+     */
+    public static function provideInvalidValue(): array
+    {
+        return [
+            'other symbol' => ['@'],
+            'uppercase letter' => ['Z'],
+            'lowercase letter' => ['j'],
+            'number' => ['1'],
+            'sequence' => ['abc?123'],
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * @param ValidCombinatorValue $combinator
+     *
+     * @dataProvider provideValidValue
+     */
+    public function parsesValidCombinator(string $combinator): void
+    {
+        $result = Combinator::parse(new ParserState($combinator, Settings::create()));
+
+        self::assertSame($combinator, $result->getArrayRepresentation()['value']);
+    }
+
+    /**
+     * @test
+     *
+     * @param non-empty-string $selectorComponent
+     *
+     * @dataProvider provideInvalidValue
+     */
+    public function parseThrowsExceptionWithInvalidCombinator(string $selectorComponent): void
+    {
+        $this->expectException(UnexpectedTokenException::class);
+
+        Combinator::parse(new ParserState($selectorComponent, Settings::create()));
+    }
+
+    /**
+     * @test
+     *
+     * @param ValidCombinatorValue $combinator
+     *
+     * @dataProvider provideValidValue
+     */
+    public function parsesCombinatorWithCommentBefore(string $combinator): void
+    {
+        $result = Combinator::parse(new ParserState('/*comment*/' . $combinator, Settings::create()));
+
+        self::assertSame($combinator, $result->getArrayRepresentation()['value']);
+    }
+
+    /**
+     * @test
+     *
+     * @param ValidCombinatorValue $combinator
+     *
+     * @dataProvider provideValidValue
+     */
+    public function parsesCombinatorWithCommentAfter(string $combinator): void
+    {
+        $result = Combinator::parse(new ParserState($combinator . '/*comment*/', Settings::create()));
+
+        self::assertSame($combinator, $result->getArrayRepresentation()['value']);
+    }
+
+    /**
+     * @test
+     *
+     * @param ValidCombinatorValue $combinator
+     *
+     * @dataProvider provideValidValue
+     */
+    public function parseExtractsCommentBefore(string $combinator): void
+    {
+        $result = [];
+        Combinator::parse(new ParserState('/*comment*/' . $combinator, Settings::create()), $result);
+
+        self::assertSame('comment', $result[0]->getArrayRepresentation()['contents']);
+    }
+
+    /**
+     * @test
+     *
+     * @param ValidCombinatorValue $combinator
+     *
+     * @dataProvider provideValidValue
+     */
+    public function parseExtractsCommentAfter(string $combinator): void
+    {
+        $result = [];
+        Combinator::parse(new ParserState($combinator . '/*comment*/', Settings::create()), $result);
+
+        self::assertSame('comment', $result[0]->getArrayRepresentation()['contents']);
+    }
+
+    /**
+     * @test
+     *
+     * @param ValidCombinatorValue $value
+     *
+     * @dataProvider provideValidValue
+     */
+    public function constructsWithValueProvided(string $value): void
+    {
+        $subject = new Combinator($value);
+
+        self::assertSame($value, $subject->getArrayRepresentation()['value']);
+    }
+
+    /**
+     * @test
+     *
+     * @param non-empty-string $value
+     *
+     * @dataProvider provideInvalidValue
+     */
+    public function constructorThrowsExceptionWithInvalidValue(string $value): void
+    {
+        $this->expectException(\UnexpectedValueException::class);
+
+        new Combinator($value);
+    }
+
+    /**
+     * @test
+     *
+     * @param ValidCombinatorValue $value
+     *
+     * @dataProvider provideValidValue
+     */
+    public function setValueSetsValueProvided(string $value): void
+    {
+        $subject = new Combinator('>');
+
+        $subject->setValue($value);
+
+        self::assertSame($value, $subject->getArrayRepresentation()['value']);
+    }
+
+    /**
+     * @test
+     *
+     * @param non-empty-string $value
+     *
+     * @dataProvider provideInvalidValue
+     */
+    public function setValueThrowsExceptionWithInvalidValue(string $value): void
+    {
+        $this->expectException(\UnexpectedValueException::class);
+
+        $subject = new Combinator('>');
+
+        $subject->setValue($value);
+    }
+
+    /**
+     * @test
+     *
+     * @param ValidCombinatorValue $value
+     *
+     * @dataProvider provideValidValue
+     */
+    public function getValueReturnsValueProvidedToConstructor(string $value): void
+    {
+        $subject = new Combinator($value);
+
+        $result = $subject->getValue();
+
+        self::assertSame($value, $result);
+    }
+
+    /**
+     * @test
+     *
+     * @param ValidCombinatorValue $value
+     *
+     * @dataProvider provideValidValue
+     */
+    public function getValueReturnsValueProvidedToSetValue(string $value): void
+    {
+        $subject = new Combinator('>');
+        $subject->setValue($value);
+
+        $result = $subject->getValue();
+
+        self::assertSame($value, $result);
+    }
+
+    /**
+     * @test
+     *
+     * @param ValidCombinatorValue $value
+     *
+     * @dataProvider provideValidValue
+     */
+    public function hasNoSpecificity(string $value): void
+    {
+        $subject = new Combinator($value);
+
+        self::assertSame(0, $subject->getSpecificity());
+    }
+
+    /**
+     * @test
+     *
+     * @param ValidCombinatorValue $value
+     *
+     * @dataProvider provideValidValue
+     */
+    public function renderReturnsValueProvided(string $value): void
+    {
+        $subject = new Combinator($value);
+
+        self::assertSame($value, $subject->render(OutputFormat::create()));
+    }
+
+    /**
+     * @test
+     */
+    public function getArrayRepresentationIncludesClassName(): void
+    {
+        $subject = new Combinator('>');
+
+        $result = $subject->getArrayRepresentation();
+
+        self::assertSame('Combinator', $result['class']);
+    }
+
+    /**
+     * @test
+     *
+     * @param ValidCombinatorValue $value
+     *
+     * @dataProvider provideValidValue
+     */
+    public function getArrayRepresentationIncludesValue(string $value): void
+    {
+        $subject = new Combinator($value);
+
+        $result = $subject->getArrayRepresentation();
+
+        self::assertSame($value, $result['value']);
+    }
+}
