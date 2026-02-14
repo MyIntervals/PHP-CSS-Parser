32dce4bf8690b5e1607620223f94aa6042efdded
JakeQZ <jake.github@qzdesign.co.uk>
2026-02-14 09:15:22 +0000

    [TASK] Use `Property\Declaration` in `ValueTest` DocBlock references (#1515)
    This replaces `Rule\Rule`, following #1508.

--- tests/Functional/Value/ValueTest.php
+++ tests/Functional/Value/ValueTest.php
@@ -19,7 +19,7 @@ final class ValueTest extends TestCase
     /**
      * the default set of delimiters for parsing most values
      *
-     * @see \Sabberworm\CSS\Rule\Rule::getDelimitersForPropertyValue
+     * @see \Sabberworm\CSS\Property\Declaration::getDelimitersForPropertyValue
      */
     private const DEFAULT_DELIMITERS = [',', ' ', '/'];
 
