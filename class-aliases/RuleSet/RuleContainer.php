90549196ed7ffc668c76c4de9d84d3ce1ce72bbe
Oliver Klee <github@oliverklee.de>
2026-02-27 16:39:15 +0100

    [BUGFIX] Avoid double autoloading of class aliases

---
+++ class-aliases/RuleSet/RuleContainer.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sabberworm\CSS\RuleSet;
+
+use function Safe\class_alias;
+
+if (!\class_exists(RuleContainer::class)) {
+    /**
+     * @deprecated in v9.2, will be removed in v10.0.  Use `DeclarationList` instead, which is a direct replacement.
+     */
+    class_alias(DeclarationList::class, RuleContainer::class);
+}
