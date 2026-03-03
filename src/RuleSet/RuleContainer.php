c83408235141c9612311087f1b12a726c1618726
Jake Hotson <jake.github@qzdesign.co.uk>
2026-03-03 18:44:00 +0000

    [TASK] Make aliased class deprecation notices visible to tools
    Resolves #1553

--- src/RuleSet/RuleContainer.php
+++ src/RuleSet/RuleContainer.php
@@ -7,8 +7,11 @@ namespace Sabberworm\CSS\RuleSet;
 use function Safe\class_alias;
 
 if (!\class_exists(RuleContainer::class, false) && !\interface_exists(RuleContainer::class, false)) {
-    /**
-     * @deprecated in v9.2, will be removed in v10.0.  Use `DeclarationList` instead, which is a direct replacement.
-     */
     class_alias(DeclarationList::class, RuleContainer::class);
+    if (!\interface_exists(RuleContainer::class, false)) {
+        /**
+         * @deprecated in v9.2, will be removed in v10.0.  Use `DeclarationList` instead, which is a direct replacement.
+         */
+        interface RuleContainer {}
+    }
 }
