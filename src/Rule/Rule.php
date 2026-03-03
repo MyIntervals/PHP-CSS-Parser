28c0b98c1bce3d5f13e539cb6d8df9dbbf39d4af
Jake Hotson <jake.github@qzdesign.co.uk>
2026-03-03 23:52:19 +0000

    Add extra `class_exists()` or `interface_exists()` check
    Add `@phpstan-ignore` which works for `booleanAnd.alwaysTrue` but not for
    `booleanNot.alwaysTrue`.

--- src/Rule/Rule.php
+++ src/Rule/Rule.php
@@ -10,7 +10,10 @@ use function Safe\class_alias;
 
 if (!\class_exists(Rule::class, false) && !\interface_exists(Rule::class, false)) {
     class_alias(Declaration::class, Rule::class);
-    if (!\class_exists(Rule::class, false)) {
+    // The test is expected to evuluate to false,
+    // but allows for the deprecation notice to be picked up by IDEs like PHPStorm
+    // @phpstan-ignore booleanNot.alwaysTrue, booleanAnd.alwaysTrue
+    if (!\class_exists(Rule::class, false) && !\interface_exists(Rule::class, false)) {
         /**
          * @deprecated in v9.2, will be removed in v10.0.  Use `Property\Declaration` instead, which is a direct
          *             replacement.
--- src/RuleSet/RuleContainer.php
+++ src/RuleSet/RuleContainer.php
@@ -8,7 +8,10 @@ use function Safe\class_alias;
 
 if (!\class_exists(RuleContainer::class, false) && !\interface_exists(RuleContainer::class, false)) {
     class_alias(DeclarationList::class, RuleContainer::class);
-    if (!\interface_exists(RuleContainer::class, false)) {
+    // The test is expected to evuluate to false,
+    // but allows for the deprecation notice to be picked up by IDEs like PHPStorm
+    // @phpstan-ignore booleanNot.alwaysTrue, booleanAnd.alwaysTrue
+    if (!\class_exists(RuleContainer::class, false) && !\interface_exists(RuleContainer::class, false)) {
         /**
          * @deprecated in v9.2, will be removed in v10.0.  Use `DeclarationList` instead, which is a direct replacement.
          */
