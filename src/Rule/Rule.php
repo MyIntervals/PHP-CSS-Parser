44130cf285ff0b5d5120d8cb2b8ac13bf0ad280b
JakeQZ <jake.github@qzdesign.co.uk>
2026-03-04 16:41:11 +0000

    [TASK] Make aliased class deprecation notices visible to tools (#1559)
    Resolves #1553

--- src/Rule/Rule.php
+++ src/Rule/Rule.php
@@ -9,9 +9,15 @@ use Sabberworm\CSS\Property\Declaration;
 use function Safe\class_alias;
 
 if (!\class_exists(Rule::class, false) && !\interface_exists(Rule::class, false)) {
-    /**
-     * @deprecated in v9.2, will be removed in v10.0.  Use `Property\Declaration` instead, which is a direct
-     *             replacement.
-     */
     class_alias(Declaration::class, Rule::class);
+    // The test is expected to evaluate to false,
+    // but allows for the deprecation notice to be picked up by IDEs like PHPStorm.
+    // @phpstan-ignore-next-line booleanAnd.alwaysTrue
+    if (!\class_exists(Rule::class, false) && !\interface_exists(Rule::class, false)) {
+        /**
+         * @deprecated in v9.2, will be removed in v10.0.  Use `Property\Declaration` instead, which is a direct
+         *             replacement.
+         */
+        class Rule {}
+    }
 }
