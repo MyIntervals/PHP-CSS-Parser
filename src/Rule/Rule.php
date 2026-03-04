2392e35d0f4d70fa38f5f7a4d1a29234fac8e5ec
Jake Hotson <jake.github@qzdesign.co.uk>
2026-03-04 16:25:15 +0000

    Fix typo and restrict `@phpstan-ignore`

--- src/Rule/Rule.php
+++ src/Rule/Rule.php
@@ -10,9 +10,9 @@ use function Safe\class_alias;
 
 if (!\class_exists(Rule::class, false) && !\interface_exists(Rule::class, false)) {
     class_alias(Declaration::class, Rule::class);
-    // The test is expected to evuluate to false,
+    // The test is expected to evaluate to false,
     // but allows for the deprecation notice to be picked up by IDEs like PHPStorm.
-    // @phpstan-ignore-next-line
+    // @phpstan-ignore-next-line booleanAnd.alwaysTrue
     if (!\class_exists(Rule::class, false) && !\interface_exists(Rule::class, false)) {
         /**
          * @deprecated in v9.2, will be removed in v10.0.  Use `Property\Declaration` instead, which is a direct
--- src/RuleSet/RuleContainer.php
+++ src/RuleSet/RuleContainer.php
@@ -8,9 +8,9 @@ use function Safe\class_alias;
 
 if (!\class_exists(RuleContainer::class, false) && !\interface_exists(RuleContainer::class, false)) {
     class_alias(DeclarationList::class, RuleContainer::class);
-    // The test is expected to evuluate to false,
+    // The test is expected to evaluate to false,
     // but allows for the deprecation notice to be picked up by IDEs like PHPStorm.
-    // @phpstan-ignore-next-line
+    // @phpstan-ignore-next-line booleanAnd.alwaysTrue
     if (!\class_exists(RuleContainer::class, false) && !\interface_exists(RuleContainer::class, false)) {
         /**
          * @deprecated in v9.2, will be removed in v10.0.  Use `DeclarationList` instead, which is a direct replacement.
