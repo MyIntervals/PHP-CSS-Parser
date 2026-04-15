dd7ef3878049123098f63f813830d9ee35f37266
Oliver Klee <github@oliverklee.de>
2026-04-15 22:28:45 +0200

    [TASK] Update the development dependencies

--- src/Rule/Rule.php
+++ src/Rule/Rule.php
@@ -8,11 +8,12 @@ use Sabberworm\CSS\Property\Declaration;
 
 use function Safe\class_alias;
 
+// @phpstan-ignore function.impossibleType
 if (!\class_exists(Rule::class, false) && !\interface_exists(Rule::class, false)) {
     class_alias(Declaration::class, Rule::class);
     // The test is expected to evaluate to false,
     // but allows for the deprecation notice to be picked up by IDEs like PHPStorm.
-    // @phpstan-ignore booleanNot.alwaysTrue, booleanNot.alwaysTrue, booleanAnd.alwaysTrue
+    // @phpstan-ignore booleanNot.alwaysTrue, booleanAnd.alwaysTrue, function.impossibleType
     if (!\class_exists(Rule::class, false) && !\interface_exists(Rule::class, false)) {
         /**
          * @deprecated in v9.2, will be removed in v10.0.  Use `Property\Declaration` instead, which is a direct
