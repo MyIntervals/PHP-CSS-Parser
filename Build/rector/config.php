92c3cfd81548ca6c9fe276cfc06881369d4c4be3
Anna Reiter <anna.reiter@oliverklee.de>
2026-04-17 12:55:35 +0200

    [CLEANUP] Move Rector configuration to `Build/`

--- Build/rector/config.php
+++ Build/rector/config.php
@@ -10,10 +10,10 @@ use Rector\Set\ValueObject\SetList;
 return RectorConfig::configure()
     ->withPaths(
         [
-            __DIR__ . '/../bin',
-            __DIR__ . '/../config',
-            __DIR__ . '/../src',
-            __DIR__ . '/../tests',
+            __DIR__ . '/../../Build',
+            __DIR__ . '/../../bin',
+            __DIR__ . '/../../src',
+            __DIR__ . '/../../tests',
         ]
     )
     ->withSets([
