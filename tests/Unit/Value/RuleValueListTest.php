d6bf39b3f3e5049931c196de130af1f4d55f4087
Oliver Klee <github@oliverklee.de>
2026-04-20 18:58:50 +0200

    [TASK] Add tests for `RuleValueList::getArrayRepresentation()` (#1577)
    Part of #1440.

--- tests/Unit/Value/RuleValueListTest.php
+++ tests/Unit/Value/RuleValueListTest.php
@@ -6,6 +6,7 @@ namespace Sabberworm\CSS\Tests\Unit\Value;
 
 use PHPUnit\Framework\TestCase;
 use Sabberworm\CSS\Value\RuleValueList;
+use Sabberworm\CSS\Value\Size;
 
 /**
  * @covers \Sabberworm\CSS\Value\RuleValueList
@@ -24,5 +25,61 @@ final class RuleValueListTest extends TestCase
         $result = $subject->getArrayRepresentation();
 
         self::assertSame('RuleValueList', $result['class']);
+    }
+
+    /**
+     * @test
+     */
+    public function getArrayRepresentationIncludesStringComponent(): void
+    {
+        $subject = new RuleValueList();
+        $subject->addListComponent('Helvetica');
+
+        $result = $subject->getArrayRepresentation();
+
+        self::assertSame('Helvetica', $result['components'][0]['value']);
+    }
+
+    /**
+     * @test
+     */
+    public function getArrayRepresentationIncludesValueComponent(): void
+    {
+        $subject = new RuleValueList();
+        $subject->addListComponent(new Size(1));
+
+        $result = $subject->getArrayRepresentation();
+
+        self::assertSame('Size', $result['components'][0]['class']);
+    }
+
+    /**
+     * @test
+     */
+    public function getArrayRepresentationIncludesMultipleMixedComponents(): void
+    {
+        $subject = new RuleValueList();
+        $subject->addListComponent(new Size(1));
+        $subject->addListComponent('+');
+        $subject->addListComponent(new Size(2));
+
+        $result = $subject->getArrayRepresentation();
+
+        self::assertSame('Size', $result['components'][0]['class']);
+        self::assertSame('+', $result['components'][1]['value']);
+        self::assertSame('Size', $result['components'][2]['class']);
+    }
+
+    /**
+     * @test
+     */
+    public function getArrayRepresentationIncludesSeparator(): void
+    {
+        $separator = ', ';
+        $subject = new RuleValueList($separator);
+
+        $result = $subject->getArrayRepresentation();
+
+        self::assertSame($separator, $result['separator']);
     }
 }
