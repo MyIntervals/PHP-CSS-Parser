00ccf833b4fc3426f9124982df3b92490b1c5752
Oliver Klee <github@oliverklee.de>
2026-05-03 11:39:35 +0200

    [TASK] Test `LineName::getArrayRepresentation()`
    The tests are basically the same as those for the parent class
    `ValueList`. The main difference is that the default separator
    now is a space, not a comma.
    
    Part of #1440.

--- tests/Unit/Value/LineNameTest.php
+++ tests/Unit/Value/LineNameTest.php
@@ -6,6 +6,7 @@ namespace Sabberworm\CSS\Tests\Unit\Value;
 
 use PHPUnit\Framework\TestCase;
 use Sabberworm\CSS\Value\LineName;
+use Sabberworm\CSS\Value\Size;
 
 /**
  * @covers \Sabberworm\CSS\Value\LineName
@@ -17,12 +18,62 @@ final class LineNameTest extends TestCase
     /**
      * @test
      */
-    public function getArrayRepresentationThrowsException(): void
+    public function getArrayRepresentationIncludesClassName(): void
     {
-        $this->expectException(\BadMethodCallException::class);
-
         $subject = new LineName();
 
-        $subject->getArrayRepresentation();
+        $result = $subject->getArrayRepresentation();
+
+        self::assertSame('LineName', $result['class']);
+    }
+
+    /**
+     * @test
+     */
+    public function getArrayRepresentationIncludesStringComponent(): void
+    {
+        $subject = new LineName(['Helvetica']);
+
+        $result = $subject->getArrayRepresentation();
+
+        self::assertSame('Helvetica', $result['components'][0]['value']);
+    }
+
+    /**
+     * @test
+     */
+    public function getArrayRepresentationIncludesValueComponent(): void
+    {
+        $subject = new LineName([new Size(1)]);
+
+        $result = $subject->getArrayRepresentation();
+
+        self::assertSame('Size', $result['components'][0]['class']);
+    }
+
+    /**
+     * @test
+     */
+    public function getArrayRepresentationIncludesMultipleMixedComponents(): void
+    {
+        $subject = new LineName([new Size(1), '+', new Size(2)]);
+
+        $result = $subject->getArrayRepresentation();
+
+        self::assertSame('Size', $result['components'][0]['class']);
+        self::assertSame('+', $result['components'][1]['value']);
+        self::assertSame('Size', $result['components'][2]['class']);
+    }
+
+    /**
+     * @test
+     */
+    public function getArrayRepresentationIncludesSpaceSeparator(): void
+    {
+        $subject = new LineName();
+
+        $result = $subject->getArrayRepresentation();
+
+        self::assertSame(' ', $result['separator']);
     }
 }
