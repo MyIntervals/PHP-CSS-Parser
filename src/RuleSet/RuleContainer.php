9218dbceec78938b63096179a4d0f43e0f9c8032
Oliver Klee <github@oliverklee.de>
2026-06-17 17:59:54 +0200

    [TASK] Update the development dependencies (#1566)

--- src/RuleSet/RuleContainer.php
+++ src/RuleSet/RuleContainer.php
@@ -6,11 +6,12 @@ namespace Sabberworm\CSS\RuleSet;
 
 use function Safe\class_alias;
 
+// @phpstan-ignore function.impossibleType
 if (!\class_exists(RuleContainer::class, false) && !\interface_exists(RuleContainer::class, false)) {
     class_alias(DeclarationList::class, RuleContainer::class);
     // The test is expected to evaluate to false,
     // but allows for the deprecation notice to be picked up by IDEs like PHPStorm.
-    // @phpstan-ignore booleanNot.alwaysTrue, booleanNot.alwaysTrue, booleanAnd.alwaysTrue
+    // @phpstan-ignore booleanNot.alwaysTrue, booleanAnd.alwaysTrue, function.impossibleType
     if (!\class_exists(RuleContainer::class, false) && !\interface_exists(RuleContainer::class, false)) {
         /**
          * @deprecated in v9.2, will be removed in v10.0.  Use `DeclarationList` instead, which is a direct replacement.
