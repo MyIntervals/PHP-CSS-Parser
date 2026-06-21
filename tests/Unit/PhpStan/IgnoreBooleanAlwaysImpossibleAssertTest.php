98b6b6eba4f542a6e5c86e2ac645c97759d9dda0
Oliver Klee <github@oliverklee.de>
2026-06-21 20:31:19 +0200

    [TASK] Update the development dependencies (#1598)

--- tests/Unit/PhpStan/IgnoreBooleanAlwaysImpossibleAssertTest.php
+++ tests/Unit/PhpStan/IgnoreBooleanAlwaysImpossibleAssertTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Sabberworm\CSS\Tests\Unit\PhpStan;
 
 use PHPStan\Analyser\IgnoreErrorExtension;
-use PHPStan\Testing\RuleTestCase;
 use PHPStan\Rules\Comparison\ImpossibleCheckTypeFunctionCallRule;
 use PHPStan\Rules\Rule;
 
