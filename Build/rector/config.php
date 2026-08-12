73ce9ee9ea402c554559d6fc6d9a45c19125c571
Oliver Klee <github@oliverklee.de>
2026-08-12 10:20:29 +0200

    [TASK] Update to Rector 2.6.2
    https://github.com/rectorphp/rector/releases/tag/2.6.2
    
    Also switch to Composer-based sets.

--- Build/rector/config.php
+++ Build/rector/config.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
 return RectorConfig::configure()
@@ -15,11 +13,8 @@ return RectorConfig::configure()
             __DIR__ . '/../../tests',
         ]
     )
+    ->withComposerBased()
     ->withSets([
-        // Rector sets
-
-        LevelSetList::UP_TO_PHP_72,
-
         // SetList::CODE_QUALITY,
         // SetList::CODING_STYLE,
         // SetList::DEAD_CODE,
@@ -28,10 +23,5 @@ return RectorConfig::configure()
         // SetList::NAMING,
         // SetList::PRIVATIZATION,
         SetList::TYPE_DECLARATION,
-
-        // PHPUnit sets
-
-        PHPUnitSetList::PHPUNIT_80,
-        // PHPUnitSetList::PHPUNIT_CODE_QUALITY,
     ])
     ->withImportNames(true, true, false);
