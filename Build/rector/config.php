c47122551b40470d4042a9c0a590daa75984b161
Oliver Klee <github@oliverklee.de>
2026-08-13 11:12:13 +0200

    [TASK] Use the PHP version from `composer.json` for Rector (#1611)
    https://getrector.com/documentation/set-lists#content-php-sets

--- Build/rector/config.php
+++ Build/rector/config.php
@@ -13,6 +13,7 @@ return RectorConfig::configure()
             __DIR__ . '/../../tests',
         ]
     )
+    ->withPhpSets()
     ->withComposerBased()
     ->withSets([
         // SetList::CODE_QUALITY,
